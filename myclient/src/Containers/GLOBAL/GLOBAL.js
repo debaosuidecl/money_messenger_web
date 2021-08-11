98e7ce6cfddbcf2fb75b96448db0229666903d3e
Osuide Osedebamen <debaosuidecl@gmail.com>
2021-08-11 04:05:50 +0100

    D

--- myclient/src/Containers/GLOBAL/GLOBAL.js
+++ myclient/src/Containers/GLOBAL/GLOBAL.js
@@ -4,16 +4,8 @@ import React, { Component } from "react";
 class GLOBAL extends Component {
   static domainMain =
     process.env.NODE_ENV === "production"
-      ? "http://159.89.55.0:8080"
+      ? "https://powersms-9b6t6.ondigitalocean.app/"
       : "http://localhost:8080";
-  static domainDomains =
-    process.env.NODE_ENV === "production"
-      ? "http://159.89.55.0:8081"
-      : "http://localhost:8081";
-  static domainuserphoneupload =
-    process.env.NODE_ENV === "production"
-      ? "http://159.89.55.0:8082"
-      : "http://localhost:8082";
   static uploadlead1 =
     process.env.NODE_ENV === "production"
       ? "http://159.89.55.0:9200"
