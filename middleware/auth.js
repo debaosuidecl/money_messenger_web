3ec948f9cf993ee0da2edc76265f5a39008202bf
Osuide Osedebamen <debaosuidecl@gmail.com>
2021-08-11 04:35:33 +0100

    D

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const { findoneuser } = require("../services/user.service");
 const { errorreturn } = require("../utils/returnerrorschema");
-
+const dotenv = require("dotenv")
+dotenv.config()
 module.exports = async function (req, res, next) {
   //Get Token from header
   const token = req.header("x-auth-token"); // extract token from the request header
@@ -15,7 +16,7 @@ module.exports = async function (req, res, next) {
 
   try {
     // we have to decode token
-    const decoded = jwt.verify(token, config.get("jwtSecret")); // decoded token
+    const decoded = jwt.verify(token, process.env.jwtSecret); // decoded token
     req.user = decoded.user; // the structure of the decoded can be find in jwt.io we can extract our user id from the decoded json
     // console.log('going next');
 
